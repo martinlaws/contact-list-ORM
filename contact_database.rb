763d9ba66777341aeaa153ae79097a47daaed6ac
Martin Laws <martinblaws@gmail.com>
2015-10-26 22:17:56 +0000

    list option now functional

--- contact_database.rb
+++ contact_database.rb
@@ -5,9 +5,22 @@ require 'csv'
 class ContactDatabase
 
   def initialize
+    choice = gets.chomp.to_sym
+    
+    case choice
+    when :new
+    when :list
+      list
+    when :show
+    when :find
+    end
+  end
+
+  def list
     contacts = CSV.read('contacts.csv')
     puts contacts
   end
+
   # CSV.open('contacts.csv', 'a') do |contact|
   # contact.array.each do |row_array|
   #   contact << row_array
