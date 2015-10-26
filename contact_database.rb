1a7681cc85e6860ed00390d8874d83d9b4dbe828
Martin Laws <martinblaws@gmail.com>
2015-10-26 22:07:06 +0000

    added CSV read functionality

--- contact_database.rb
+++ contact_database.rb
@@ -1,5 +1,15 @@
 ## TODO: Implement CSV reading/writing
 require 'csv'
+# require_relative './contacts.csv'
 
 class ContactDatabase
+
+  def initialize
+    contacts = CSV.read('contacts.csv')
+    puts contacts
+  end
+  # CSV.open('contacts.csv', 'a') do |contact|
+  # contact.array.each do |row_array|
+  #   contact << row_array
+  # end
 end
--- contact_list.rb
+++ contact_list.rb
@@ -1,5 +1,12 @@
-require_relative 'contact'
-require_relative 'contact_database'
+require_relative './contact'
+require_relative './contact_database'
 
 # TODO: Implement command line interaction
 # This should be the only file where you use puts and gets
+class Application
+  def initialize
+    ContactDatabase.new
+  end
+end
+
+Application.new
